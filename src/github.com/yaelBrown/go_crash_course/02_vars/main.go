c55f8de5eb11bcef546cc2f67368f6bcdebece7f
Yael Brown <yaelrbrown@gmail.com>
2019-01-03 16:32:28 -0500

    Initial Push
    initial push

---
+++ src/github.com/yaelBrown/go_crash_course/02_vars/main.go
@@ -0,0 +1,27 @@
+package main
+
+// methods to print stuff on the screen
+import "fmt"
+
+func main() {
+  // MAIN TYPES
+  // string
+  // bool
+  // int
+  // int int8 int16 int32 int64
+  // uint uint8 uint16 uint32 uint64 uintptr
+  // byte - alias for uint8
+  // rune - alias for int32
+  // float32 float64
+  // complex64 comples128
+
+  // Create a variable you have to use it. 
+
+  // Using var (no semi colons)
+  var name string = "Yael"
+  var age = 32
+
+  fmt.Println(name, age)
+
+
+}
